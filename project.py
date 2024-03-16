f5b4235a5186845e2c89c1b18351d06ebff4515f
MuhammedTahircanYilmaz <mtahircanyilmaz@gmail.com>
2024-03-16 14:44:31 +0300

    "ID field added"

--- project.py
+++ project.py
@@ -1,17 +1,24 @@
 import csv
+import json
+import os
+
 
 class Personnel:
     positions = ["employee", "admin"]
-    departments =  ["HR", "SoftwareDev", "Marketing", "Design", "IT", "QualityAssurance", "ProductManager", "Accounting"]
+    departments = ["HR", "SoftwareDev", "Marketing", "Design", "IT", "QualityAssurance", "ProductManager", "Accounting"]
 
-    def __init__(self, name=None, position=None, department=None, wage=0):
-        self.name = name
-        self.position = position
-        self.department = department
-        self.wage = wage
-    
+    def __init__(self, id_number=None, name=None, position=None, department=None, wage=0):
+        self._id_number = id_number
+        self._name = name
+        self._position = position
+        self._department = department
+        self._wage = wage
+
     def __str__(self):
-        return f"{self.name}, {self.position}, {self.department}, {self.wage}"
+        return f"{self.id_number}, {self.name}, {self.position}, {self.department}, {self.wage}"
+
+    def id_number(self):
+        return self.id_number
 
     @property
     def position(self):
@@ -20,41 +27,46 @@ class Personnel:
     @position.setter
     def position(self, position):
         if position not in Personnel.positions and position != None:
-            print("You have entered a wrong position, try again")
+            print("You have entered an invalid position, please try again.")
+            return 0
         else:
             self._position = position
-
 
     @property
     def wage(self):
         return self._wage
-    
+
     @wage.setter
     def wage(self, wage):
-        if wage < 0:
-            raise ValueError
-        else:
-            self._wage = wage
-
+        try:
+            wage = int(wage)
+            if wage < 0:
+                print("You have entered an invalid Wage. Please try again.")
+                return 0
+            else:
+                self._wage = wage
+        except ValueError:
+            print("You have entered an invalid value. Please try again.")
+            return 0
 
     @property
     def department(self):
         return self._department
-    
+
     @department.setter
     def department(self, department):
         if department not in Personnel.departments and department != None:
-            print("You have entered an incorrect department. Please try again")
-        else: 
+            print("You have entered an invalid department. Please try again.")
+            return 0
+        else:
             self._department = department
-
 
     @property
     def name(self):
         return self._name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str):
         if not name.isalpha() and name != None:
             print("You have entered an invalid name")
         else:
@@ -62,43 +74,78 @@ class Personnel:
 
 
 def main():
+    db_file = "personnel_database.csv"
+    id_file = "id_count.json"
+
+    if not os.path.exists(db_file) or os.path.getsize(db_file) == 0:
+        with open(db_file, "w") as f:
+            writer = csv.DictWriter(f, fieldnames=["Name", "Position", "Department", "Wage"])
+            writer.writeheader()
+
+    if not os.path.exists(id_file) or os.path.getsize(db_file) == 0:
+        with open(id_file, "w") as j:
+            id_dict = {"current_id": 0}
+            json.dump(id_dict, j, indent=4)
+            last_id_number = 0
+    else:
+        with open(id_file, "r") as j:
+            data = json.load(j)
+            last_id_number = data["current_id"]
+
     while True:
         action = (input("Please enter the action you want to conduct: ")).lower()
 
         if action == "add":
-            add_personnel()
+            add_personnel(last_id_number)
         elif action == "remove":
             remove_personnel()
         elif action == "update":
             update_personnel()
 
 
-def add_personnel():
+def add_personnel(last_id_number):
+    id_number = last_id_number + 1
+    person = Personnel()
     while True:
         name = input("Please enter the name of the personnel: ")
-        person = Personnel(name)
+        person.name = name
+
         if person.name == name:
             break
     while True:
         position = (input("Please enter the position of the personnel: ")).lower()
         if position == "admin":
             person.position = position
-            break 
+            break
         else:
             person.position = position
             if person.position == position:
-                while True:    
+                while True:
                     department = input("Please enter the department of the personnel: ")
                     person.department = department
                     if person.department == department:
                         break
             break
     while True:
-        wage = int(input("Please enter the wage of the personnel: "))
+        wage = input("Please enter the wage of the personnel: ")
         person.wage = wage
-        if person.wage == wage:
-            break       
-    print (person)
+        if person.wage == int(wage):
+            break
+
+    person.id_number = id_number
+
+    with open("personnel_database.csv", "a") as db:
+        writer = csv.DictWriter(db, fieldnames=["ID", "Name", "Position", "Department", "Wage"])
+        person_dict = {"ID": person.id_number, "Name": person.name, "Position": person.position,
+                       "Department": person.department, "Wage": person.wage}
+
+        writer.writerow(person_dict)
+
+    with open("id_count.json", "w") as j:
+        id_dict = {"current_id": id_number}
+        json.dump(id_dict, j, indent=4)
+
+
 def remove_personnel():
     ...
 
@@ -109,7 +156,6 @@ def update_personnel():
 
 if __name__ == "__main__":
     main()
-
 
 # personel registration program
 # create general class of staff
